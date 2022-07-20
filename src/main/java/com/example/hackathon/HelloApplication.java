3604b6719c87260074ab9ada1123e8e83feb327c
Vibhu Iyer <viiyer@viiyer-rlfwtmxv5p.guidewire.com>
2022-07-20 10:28:06 -0400

    idk

--- src/main/java/com/example/hackathon/HelloApplication.java
+++ src/main/java/com/example/hackathon/HelloApplication.java
@@ -4,12 +4,20 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.stream.Collectors;
 
 public class HelloApplication extends Application {
     @Override
     public void start (Stage stage) throws IOException {
+        File file = new File("src/main/resources/com/example/hackathon/people.json");
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String s = reader.lines().collect(Collectors.joining(" "));
+//        HashMap<String, String> peopleString = GsonBuilder
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
---
+++ src/main/java/com/example/hackathon/Person.java
@@ -0,0 +1,9 @@
+package com.example.hackathon;
+
+import javafx.scene.image.Image;
+
+public class Person {
+    boolean isFlagged, isInPrevious;
+    String name;
+    Image image;
+}
